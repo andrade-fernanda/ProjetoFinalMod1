2671f0e2e9ae317e242390e26fa0d949c0154d39
andrade-fernanda <fvw.andrade@gmail.com>
2022-07-31 18:17:32 -0300

    Projeto Final Bloco 1

---
+++ projetofinalMod1/ProntuarioEletronico.java
@@ -0,0 +1,20 @@
+package projetofinalMod1;
+
+public class ProntuarioEletronico 
+{
+
+	public static void main(String[] args) 
+	{
+//		AgenteComunitario cadastro1 = new AgenteComunitario("Marina Rosa", "ACS");
+			//	Enfermagem pct1 = new Enfermagem("Drª Fernanda Andrade", "Enfermeira Chefe");
+			//	MedClinic pct2 = new MedClinic ("Drª Cinthia Santos","Médica Clínica Geral");
+				
+				
+			
+				
+	}
+
+
+}
+
+
---
+++ projetofinalMod1/UBS.java
@@ -0,0 +1,12 @@
+package projetofinalMod1;
+
+public interface UBS 
+{
+	// MÉTODOS DA INTERFACE
+	
+			void temperatura(double testeTemperatura);
+			void avisoTemperatura();
+			void cartaoVac(boolean CartaoVac);
+				
+
+}
